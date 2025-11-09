21e06e61d0691b868bf5beda9bc8b9e6249d68da
javier.otero <javier.otero@alumnos.upm.es>
2025-11-09 19:29:46 +0100

    09.11

--- Entrega 2 ej2.py
+++ Entrega 2 ej2.py
@@ -12,7 +12,7 @@ N_nodos = 20
 N_productos = 10
 Prob_arco = 0.15
 
-random.seed(0)  #LUEGO QUITAR
+#random.seed(0)  #LUEGO QUITAR
 
 
 nodos = list(range(1, N_nodos + 1))
@@ -64,4 +64,69 @@ for p in productos:
 
 print("origenes:", Start)
 print("Destinos: ", End)
+
+#Problema
+
+model = lp.LpProblem("Entrega2 ej2", lp.LpMinimize)
+
+#Variables
+
+x = lp.LpVariable.dicts("x", [(p, a) for p in productos for a in arcos], lowBound=0, cat="Continuous" )
+
+y = lp.LpVariable.dicts("y", arcos, lowBound=0, upBound=1, cat="Binary")
+
+#Función objetivo
+
+CF = lp.lpSum(Cf[a] * y[a] for a in arcos)
+
+CV = lp.lpSum(Cv[a] * x[(p,a)] for p in productos for a in arcos )
+
+model += CF + CV
+
+
+#Restricciones
+
+for p in productos:
+    for i in nodos:
+        salida = lp.lpSum(x[(p,a)] for a in arcos if a[0] == i)
+        entrada = lp.lpSum(x[(p,a)] for a in arcos if a[1] == i)
+        
+        if i == Start[p]:
+            balance = 1
+        elif i == End[p]:
+            balance = -1
+        else:
+            balance = 0
+            
+        
+        model += salida - entrada == balance
+
+     # Si y=0 no puede haber flujo
+     
+for (i, j) in arcos:
+    model += lp.lpSum(x[p, (i, j)] for p in productos) <= 100 * y[(i, j)]
+    
+
+#Resolver
+
+model.solve()
+
+print("\n Estado del problema: ", lp.LpStatus[model.status])
+print("\n Objetivo: ", lp.value(model.objective))
+
+
+print("\n Arcos abiertos:")
+for a in arcos:
+    if y[a].varValue > 0.001:
+        print(f"  {a}: abierto")
+
+print("\n Flujos por producto:")
+for p in productos:
+    print(f"Producto {p}:")
+    for a in arcos:
+        val = x[(p, a)].varValue
+        if val and val > 1e-6:
+            print(f"  Flujo {val:.2f} por arco {a}")
+
+
 
